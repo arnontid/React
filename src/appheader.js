06bb6b8441e821a8fe017481445da52df057b9b3
Arnontid <615021000620@mail.rmutk.ac.th>
2021-02-17 12:29:13 +0700

    rc

---
+++ src/appheader.js
@@ -0,0 +1,12 @@
+function appheader() {
+    
+    return (
+       
+          <header className="App-header">
+              <img className ="header-logo" src ="/pic/2.jpg"/> 
+    
+           
+          </header>
+          );
+    }
+    export default appheader;
---
+++ src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/1.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/2.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/3.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/4.jpg"
+      
+    
+    }
+  ];
+  export default items;
